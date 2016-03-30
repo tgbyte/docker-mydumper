eaa20911da9747a4da21c4d5711e06a65d2f3659
Thilo-Alexander Ginkel <thilo@ginkel.com>
2016-03-30 23:55:42 +0200

    Fix working directory

--- entrypoint.d/credentials.sh
+++ entrypoint.d/credentials.sh
@@ -4,5 +4,6 @@ MYSQL_USER=\"${MYSQL_USER}\"
 MYSQL_PASSWORD=\"${MYSQL_PASSWORD}\"
 MYDUMPER_THREADS=${MYDUMPER_THREADS}
 
+cd /backups
 mydumper -v 3 -h $MYSQL_HOST -u $MYSQL_USER -p $MYSQL_PASSWORD -t $MYDUMPER_THREADS -c --less-locking --use-savepoints" > /run-mydumper.sh
 chmod +x /run-mydumper.sh
