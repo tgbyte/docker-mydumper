d6e9b33ce938d480bfc8ffaeca21d53401b82def
Thilo-Alexander Ginkel <thilo@ginkel.com>
2016-06-20 14:44:09 +0200

    Expire backups older than 7 days

--- entrypoint.d/credentials.sh
+++ entrypoint.d/credentials.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-echo "MYSQL_HOST=\"${MYSQL_HOST}\"
+echo "#!/bin/bash -e
+MYSQL_HOST=\"${MYSQL_HOST}\"
 MYSQL_USER=\"${MYSQL_USER}\"
 MYSQL_PASSWORD=\"${MYSQL_PASSWORD}\"
 MYDUMPER_THREADS=${MYDUMPER_THREADS}
 
 cd /backups
-mydumper -v 3 -h $MYSQL_HOST -u $MYSQL_USER -p $MYSQL_PASSWORD -t $MYDUMPER_THREADS -c --less-locking --use-savepoints" > /run-mydumper.sh
+mydumper -v 3 -h $MYSQL_HOST -u $MYSQL_USER -p $MYSQL_PASSWORD -t $MYDUMPER_THREADS -c --less-locking --use-savepoints
+find /backups/ -mindepth 1 -mtime +7 -delete
+" > /run-mydumper.sh
 chmod +x /run-mydumper.sh
